494e9fd36b6b96725d62bdc603166713794dbb22
lev-s <naiman.sk.jr@gmail.com>
2021-12-31 11:34:39 +0600

    Updates

---
+++ precache-manifest.9f2d3894fb2151b8d44d56abe2ff5284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f484122a2f11499854eb3522168a6e",
+    "url": "/tic-tac-toe-reactjs/index.html"
+  },
+  {
+    "revision": "bf37afb4da20b9e68784",
+    "url": "/tic-tac-toe-reactjs/static/css/main.a7565a89.chunk.css"
+  },
+  {
+    "revision": "c5fcff42bf7081fbff75",
+    "url": "/tic-tac-toe-reactjs/static/js/2.61e06272.chunk.js"
+  },
+  {
+    "revision": "bf37afb4da20b9e68784",
+    "url": "/tic-tac-toe-reactjs/static/js/main.bdb83b00.chunk.js"
+  },
+  {
+    "revision": "1359dfbe2ed280cc2b19",
+    "url": "/tic-tac-toe-reactjs/static/js/runtime~main.67d42337.js"
+  }
+]);
